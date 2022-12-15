357d31f0d762db8c4f651f64f2bb6c7abda7da74
wyuh <kakahuy99@gmail.com>
2022-12-15 16:32:45 +0000

    test

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -13,7 +13,7 @@ outdir="${script_dirname}/_output"
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
 -boot c -m 8000M -hda "${outdir}/rootfs/focal-server-cloudimg-amd64.img" \
 -net user \
--smp 8 \
+-smp 16 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
 -nic user,hostfwd=tcp::2222-:22,hostfwd=tcp::1194-:1194 \
 -serial mon:stdio -display none
