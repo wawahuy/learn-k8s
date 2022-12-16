eddc6c4fd60aeea2648d8967529da8cae912d0ba
wyuh <kakahuy99@gmail.com>
2022-12-16 05:05:19 +0000

    done master

--- .gitpod/master-token/run.sh
+++ .gitpod/master-token/run.sh
@@ -2,13 +2,14 @@
 
 ABSOLUTE_PATH_MN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source $ABSOLUTE_PATH_MN/common/support-os.sh
-source $ABSOLUTE_PATH_MN/common/sudo.sh
+source $ABSOLUTE_PATH_MN/../common/support-os.sh
+source $ABSOLUTE_PATH_MN/../common/sudo.sh
 
 ABSOLUTE_PATH_MN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd $ABSOLUTE_PATH_MN
 
+$SUDO curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 $SUDO apt install nodejs -y
 npm install
 npm run run
